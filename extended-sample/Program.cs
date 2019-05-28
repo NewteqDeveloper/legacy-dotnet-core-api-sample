9cdc4834d908be34c0f5ab6a1279ad50de1df500
Newteq <newt.fourie@entelect.co.za>
2019-05-28 12:44:06 +0200

    some more clean up and commenting

---
+++ extended-sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using ExtendedSample.Hosting;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ExtendedSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var builder = new HostBuilder();
+            builder.Build<Startup>().Run();
+        }
+    }
+}
