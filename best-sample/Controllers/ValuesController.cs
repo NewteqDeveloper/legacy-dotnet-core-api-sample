e082c6c90e624bdf767df0889e8814b00fd740f9
Newteq <newt.fourie@entelect.co.za>
2019-05-28 15:57:48 +0200

    make use of the new settings value

--- best-sample/Controllers/ValuesController.cs
+++ best-sample/Controllers/ValuesController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Domain.Configuration.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BestSample.Controllers
 {
@@ -10,11 +8,24 @@ namespace BestSample.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly ISettings settings;
+
+        public ValuesController(ISettings settings)
+        {
+            this.settings = settings;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", this.settings.Complex.Value1, this.settings.Complex.Value2 };
         }
 
         // GET api/values/5
@@ -33,12 +44,6 @@ namespace BestSample.Controllers
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
         {
         }
     }
